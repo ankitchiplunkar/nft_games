232022d4c3c407d7269dba073ef8f9566a315654
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2022-07-30 12:02:14 +0100

    minor updates

--- contracts/Ahab.sol
+++ contracts/Ahab.sol
@@ -12,10 +12,11 @@ contract Ahab is ERC20, AccessControl, ReentrancyGuard {
     bytes32 public constant KILLER_ROLE = keccak256("KILLER_ROLE");
     bytes32 public constant HEALER_ROLE = keccak256("HEALER_ROLE");
     address public creator;
-
+    uint public constant PRECISION = 100;
+    mapping(address => uint) public numPlayers;
     Royale public constant ROYALE = Royale(0x8e094bC850929ceD3B4280Cc031540A897F39706);
 
-    constructor() ERC20("AHAB", "AHB") {
+    constructor() ERC20("AHAB", "AHA") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(KILLER_ROLE, msg.sender);
         _setupRole(HEALER_ROLE, msg.sender);
@@ -24,12 +25,12 @@ contract Ahab is ERC20, AccessControl, ReentrancyGuard {
 
     // returns valuation of a player
     function valuation(uint player) public view returns(uint) {
-        return ROYALE.getHP(player) + ROYALE.getAP(player)/2;
+        return ROYALE.getHP(player)*PRECISION + ROYALE.getAP(player)*PRECISION/2;
     }
 
     // returns current epoch of the game
     function epoch() public view returns(uint) {
-        return (block.timestamp - ROYALE.gameStartTime()) / 6 hours;
+        return (block.timestamp - ROYALE.gameStartTime())*PRECISION / 6 hours;
     }
 
     // receive erc721 from msg sender and receive equivalent amount of erc20
@@ -37,26 +38,31 @@ contract Ahab is ERC20, AccessControl, ReentrancyGuard {
     function enter(uint player) public nonReentrant {
         uint value = valuation(player);
         ROYALE.transferFrom(msg.sender, address(this), player);
-        _mint(msg.sender, value**decimals());
+        numPlayers[msg.sender]++;
+        _mint(msg.sender, value*10**decimals());
     }
 
     // receive erc20 from msg sender and send erc721 of less or equal value
-    // also revoke member role
+    // send the worst players in the DAO to the msg.sender
+    // take senders all AHA tokens and send them number of BRP tokens they minted
+    // the current valuation of AHA tokens burnt should be greater than the valuation of BRP tokens sent back 
     function exit(uint player) public nonReentrant {
         uint value = valuation(player);
-        uint currentValue = value + epoch();
         // burn will make sure that player has more than current value of tokens
-        _burn(msg.sender, currentValue**decimals());
+        _burn(msg.sender, value*10**decimals());
         ROYALE.transferFrom(address(this), msg.sender, player);
     }
 
     // harms another player using an array of attack players and their APs
     // also make sure that we are not killing dao players
+    // this needs a try catch as well
     // care should be given while calling this contract since player can stop existing in between the call
     function kill(uint player, uint[] calldata attackPlayers, uint[] calldata attackAP) public onlyRole(KILLER_ROLE) {
-        require(ROYALE.ownerOf(player) != address(this), "cannot attack dao members");
-        require(attackPlayers.length == attackAP.length, "lengths mismatch");
+        require(ROYALE.ownerOf(player) != address(this), "Cannot attack dao members");
+        require(attackPlayers.length == attackAP.length, "Lengths mismatch");
+        // need to handle multiple players
         for (uint x; x < attackPlayers.length; x++) {
+            // need to make sure that we can handle reverts in here
             ROYALE.attack(attackPlayers[x], player, attackAP[x]);
         }
     }
@@ -83,7 +89,7 @@ contract Ahab is ERC20, AccessControl, ReentrancyGuard {
     // if the game has ended
     // erc20 from user is claimed and eth winnings are returned pro-rata
     function claim() public nonReentrant {
-        require(ROYALE.players() == 1, "Game is stil on");
+        require(ROYALE.players() == 1, "Game is stil on!");
         uint256 userBalance = balanceOf(msg.sender);
         // burn will make sure that user does not claim again
         _burn(msg.sender, userBalance);
